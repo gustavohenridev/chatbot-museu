660308cfc67cea6c3ca1a02d99ce4ce77aa3b44f
gustavohenridev <gustavohenri.dev@gmail.com>
2025-03-14 14:53:53 -0300

    text (2:13:10)

--- src/app/components/chat-dialog/chat-dialog.component.ts
+++ src/app/components/chat-dialog/chat-dialog.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { Message } from '../../types/message.type';
 import { ChatResponseComponent } from "../chat-response/chat-response.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'chat-dialog',
   standalone: true,
   imports: [
-    ChatResponseComponent
+    ChatResponseComponent,
+    CommonModule
 
   ],
   templateUrl: './chat-dialog.component.html',
